3202a56b10cbf3ce4c59898c51bc8ebb0959ee18
Megumin <nezukokawai27@gmail.com>
2025-01-27 18:37:45 +0000

    X6739: Welcome To Pixel OS
    Signed-off-by: Megumin <nezukokawai27@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X6739.mk
+    $(LOCAL_DIR)/aosp_X6739.mk
--- aosp_X6739.mk
+++ aosp_X6739.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6739/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
-PRODUCT_NAME := lineage_X6739
+PRODUCT_NAME := aosp_X6739
 PRODUCT_DEVICE := X6739
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -26,3 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X6739 \
     BuildFingerprint=Infinix/X6739-GL/Infinix-X6739:12/SP1A.210812.016/241017V722:user/release-keys
+
+# More
+TARGET_BOOT_ANIMATION_RES := 1080
