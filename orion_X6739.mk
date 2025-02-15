bf0ffd9ede39dd1ce4900f60382ff5b1f413f5b8
Megumin <nezukokawai27@gmail.com>
2025-02-15 09:49:59 +0000

    X6739: Orion OS bring up
    Signed-off-by: Megumin <nezukokawai27@gmail.com>

--- orion_X6739.mk
+++ orion_X6739.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The OrionOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6739/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common OrionOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
-PRODUCT_NAME := lineage_X6739
+PRODUCT_NAME := orion_X6739
 PRODUCT_DEVICE := X6739
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -26,3 +26,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X6739 \
     BuildFingerprint=Infinix/X6739-GL/Infinix-X6739:12/SP1A.210812.016/241017V722:user/release-keys
+
+# Orion-ify
+ORION_MAINTAINER := Megumin27
+ORION_MAINTAINER_LINK := https://t.me/RyX33N
+ORION_BUILD_TYPE := Unofficial
+ORION_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := true
