4fcd003d3ecdd08df4f537e6d139778f69ed8d9b
Megumin <nezukokawai27@gmail.com>
2025-02-27 01:59:44 +0000

    X6739: Bliss bring up
    Signed-off-by: Megumin <nezukokawai27@gmail.com>

--- bliss_X6739.mk
+++ bliss_X6739.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The BlissRoms Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/infinix/X6739/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BlissRoms stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
-PRODUCT_NAME := lineage_X6739
+PRODUCT_NAME := bliss_X6739
 PRODUCT_DEVICE := X6739
 PRODUCT_MANUFACTURER := INFINIX
 PRODUCT_BRAND := Infinix
@@ -26,3 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-transsion
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=X6739 \
     BuildFingerprint=Infinix/X6739-GL/Infinix-X6739:12/SP1A.210812.016/241017V722:user/release-keys
+
+# Bliss-ify
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
