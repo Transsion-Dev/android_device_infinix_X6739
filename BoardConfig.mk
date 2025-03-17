a2cfd2ef817d4e911227a7589fecde7abe75c5ec
xoo2001 <ekora2001@gmail.com>
2025-03-17 21:37:20 +0700

    X6739: Build `vendor_boot` in userdebug

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,10 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     vendor
 
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
+ AB_OTA_PARTITIONS += vendor_boot
+ endif
+
 BUILD_BROKEN_PREBUILT_ELF_FILES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
